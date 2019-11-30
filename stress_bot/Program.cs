6d4da7fd6cc9bd1c7b331205e944d00b47b28b98
Seo Seong Ho <swa04246@naver.com>
2019-11-30 16:09:29 +0900

    initial commit

---
+++ stress_bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stress_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
